7f8659bf9eab580236b1a06a7268c4496a1ca07d
Scott Diallo <diallot@me.com>
2016-06-05 23:17:33 -0700

    pirate pantry populated

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
+//Global variable declarations
+var pantry = { //object containing drink options
+    strong: ["glug of rum", "slug of whiskey", "splash of gin"],
+    salty: ["olive of a stick", "splash of tonic", "twist of lemon peel"],
+    bitter: ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+    sweet: ["sugar cube", "spoonful of honey", "splash of cola"],
+    fruity: ["slice of orange", "dash of cassis", "cherry on top"]
+};
+
 $(document).ready(function () {
     //    console.log('Hello World!');
-    $('h1').hover(function () {
-        alert("Watta Guan man");
 
-    });
 
 });
