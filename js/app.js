6ee06a30c6d76d9845c48c352052f9e2d2f70c1b
Scott Diallo <diallot@me.com>
2016-06-05 23:45:23 -0700

    created construction function to make drink

--- js/app.js
+++ js/app.js
@@ -6,9 +6,31 @@ var pantry = { //object containing drink options
     sweet: ["sugar cube", "spoonful of honey", "splash of cola"],
     fruity: ["slice of orange", "dash of cassis", "cherry on top"]
 };
+var Order = function (orderValues) {
+    //getting user choices  from the DOM
+    this.strong = orderValues[0];
+    this.salty = orderValues[1];
+    this.bitter = orderValues[2];
+    this.sweet = orderValues[3];
+    this.fruity = orderValues[4];
+};
+//populating the drink
+var Drink = function (pantry, drinkOrder) {
+    var ingredientNumber,
+        ingredientsArray = [];
 
-$(document).ready(function () {
-    //    console.log('Hello World!');
+    for (var userPreference in drinkOrder) {
+        ingredientNumber = generateRandomNumber(0, 2);
+        if (drinkOrder[userPreference]) {
+            ingredientsArray.push(pantry[userPreference][ingredientNumber]);
+        }
+    }
+    return ingredientsArray;
+};
+
+var
+    $(document).ready(function () {
+        //    console.log('Hello World!');
 
 
-});
+    });
