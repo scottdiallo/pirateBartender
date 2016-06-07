c37c626a7b4a684a107a9f29dd0486212c85f43a
Scott Diallo <diallot@me.com>
2016-06-07 14:42:43 -0700

    app is finally working yeahhh

--- js/app.js
+++ js/app.js
@@ -1,22 +1,21 @@
-//Global variable declarations
-var pantry = { //object containing drink options
-    strong: ["glug of rum", "slug of whiskey", "splash of gin"],
-    salty: ["olive of a stick", "splash of tonic", "twist of lemon peel"],
+var pantry = {
+    strong: ["glug of rum", "slug of whisky", "splash of gin"],
+    salty: ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
     bitter: ["shake of bitters", "splash of tonic", "twist of lemon peel"],
     sweet: ["sugar cube", "spoonful of honey", "splash of cola"],
     fruity: ["slice of orange", "dash of cassis", "cherry on top"]
 };
 
-var Order = function (orderValues) { //constructor function
-    //getting user choices  from the DOM
+var Order = function (orderValues) {
+    // capturing user values from the select form.
     this.strong = orderValues[0];
     this.salty = orderValues[1];
     this.bitter = orderValues[2];
     this.sweet = orderValues[3];
     this.fruity = orderValues[4];
 };
-//populating the drink
-var Drink = function (pantry, drinkOrder) { //constructor function
+
+var Drink = function (pantry, drinkOrder) {
     var ingredientNumber,
         ingredientsArray = [];
 
@@ -27,95 +26,62 @@ var Drink = function (pantry, drinkOrder) { //constructor function
         }
     }
     return ingredientsArray;
-
-<<<<<<< HEAD
 };
 
 var toTitleCase = function (str) {
-    return str.replace(/\w\S*/g, function (text) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowercase();
+    return str.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     });
 }
 
-//function to generate random number
+//Math.random function
+
 var generateRandomNumber = function (min, max) {
+
+    //creating Math.random function to randomly pick ingredient
     var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
     return randomNumber;
 }
 
-//using if statement to put together name based on ingredient chosen
-var drinkNamer = function (concotion) {
-    var drinkNamerOutput = concotion[0].split(" ");
-    return "ye Sparkly " + toTitleCase(drinkNamerOutput[drinkNamerOutput.length - 1]) + " Grog";
+//creating drink namer variable
+
+var drinkNamer = function (concoction) {
+    var drinkNamerOutput = concoction[0].split(" ");
+    return "yer Sparkly " + toTitleCase(drinkNamerOutput[drinkNamerOutput.length - 1]) + " Grog";
 };
 
 $(document).ready(function () {
-    //    console.log('Hello World!');
-    $('output').hide();
 
-    $('form').on('submit', function (event) {
-        event.preventDefault();
-        // setting the empty values array
-        orerValues = []; //user choice from DOM will be push to this array
-
-        //making sure each of the ingredient have been chosen
-        $('select').each(function () {
-            orderValues.push($(this).val() === 'yes' ? true : false);
-        });
-
-        // using our two constructors to create 2 new objects
-        drinkOrder = new Order(orderValues); //create new order from user preferences
-        concotion = new Drink(pantry, drinkOrder); //randomly select value within pantry
-
-        //build the user choice from from our ingreding array
-        var buildTheHtmlOutput = "";
-        $.each(concotion, function (key, value) {
-            buildTheHtmlOutput += "<li>" + value + "</li>";
-        });
-=======
-
-
-$(function () {
-    console.log('Hello World!');
     $('.output').hide();
-    $('form').on('submit', function (event) {
-        event.preventDefault();
-        // setting the empty values array
-        orderValues = []; //user choice from DOM will be push to this array
 
-        //making sure each of the ingredient have been chosen
+
+    $('form').on('submit', function (event) {
+
+        event.preventDefault(); //using javascript to submit form when page is reload
+
+        orderValues = [];
+
+        //making sure each choice have been selected
         $('select').each(function () {
             orderValues.push($(this).val() === 'yes' ? true : false);
         });
 
-        // using our two constructors to create 2 new objects
-        drinkOrder = new Order(orderValues); //create new order from user preferences
-        concotion = new Drink(pantry, drinkOrder); //randomly select value within pantry
+        //using our 2 constructor to create the two objects
+        drinkOrder = new Order(orderValues); // create new order from DOM
+        concoction = new Drink(pantry, drinkOrder); // mix drink with Drink constructor
 
-        //build the user choice from from our ingreding array
+        //build the chosen ingredients from the ingredients array
         var buildTheHtmlOutput = "";
-        $.each(concotion, function (key, value) {
+        $.each(concoction, function (key, value) {
             buildTheHtmlOutput += "<li>" + value + "</li>";
         });
 
         //display the output container
         $('.output').show();
 
-        //populate it with the ingredients
-        $('.output ul').html(buildTheHtmlOutput);
+        $(".output ul").html(buildTheHtmlOutput);
 
-        //name the customer drink with drinkName
-        $('output h3').html("Here be " + drinkNamer(concoction) + ", ye scurvy dog!");
->>>>>>> master
-
-        display the output container
-        $('.output').show();
-
-        //populate it with the ingredients
-        $('.output ul').html(buildTheHtmlOutput);
-
-        //name the customer drink with drinkName
-        $('output h3').html("Here be " + drinkNamer(concoction) + ", ye scurvy dog!");
-
+        // naming the customer drink
+        $(".output h3").html("Here be " + drinkNamer(concoction) + ", ye scurvy dog!");
     });
 });
